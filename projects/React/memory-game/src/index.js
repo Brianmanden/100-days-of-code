7eefc878c8b32d7ba574fdebf109e8a1c52e41da
Brianmanden <brianmanden@gmail.com>
2019-12-27 19:36:37 +0100

    Refactor + add control for matching cards

--- projects/React/memory-game/src/index.js
+++ projects/React/memory-game/src/index.js
@@ -3,37 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Card(props){
-    const imageURI = process.env.PUBLIC_URL + '/assets/' + props.imagePath;
-    
+    const imageURI = process.env.PUBLIC_URL + props.imagePath;
+    const cardText = "Card " + props.index;
+
     return(
         <div
             className="card"
-            key={props.imagePath + Math.random()}
+            key={props.imagePath + "_" + props.index}
             onClick={props.onClick}>
-            <img src={imageURI} />
+            <img src={imageURI} alt={cardText} title={cardText} />
         </div>
     );
 }
 
 class Board extends React.Component{
-    renderCard(imagePath){
+    renderCard(imagePath, index){
         return(
             <Card
                 imagePath={imagePath}
-                onClick={() => this.props.onClick()}
+                key={"key_" + index}
+                index={index}
+                onClick={() => this.props.onClick(imagePath, index)}
             />
         );
     }
 
-    render(){
+    render(){ 
         return(
-            this.props.images.map((imageSrc) =>{
+            this.props.images.map((imageSrc, index) => {           
                 return(            
                     <div
                         className="board-row"
-                        key={"theCard" + imageSrc + Math.random()}
+                        key={index+"abc"}
                     >
-                        {this.renderCard(imageSrc)}
+                        {this.renderCard(imageSrc, index)}
                     </div>
                 );
             })
@@ -45,21 +48,48 @@ class Game extends React.Component{
     constructor(props){
         super(props);
         const cardImagePaths = [
-            'kitty01.png',
-            'kitty02.png',
-            'kitty03.png',
-            'kitty04.png',
-            'skeleton01.png',
+            '/assets/kitty01.png',
+            '/assets/kitty02.png',
+            '/assets/kitty03.png',
+            '/assets/kitty04.png',
+            '/assets/skeleton01.png',
         ];
         const doubleCardSet = cardImagePaths.concat(cardImagePaths);
 
         this.state = {
             cardImages: shuffleCards(doubleCardSet),
+            clickedCard: [],
+            status: ""
         };
     }
 
-    handleClick(){
-        console.log(this);
+    handleClick(cardImage, index){
+        console.log(cardImage, index, this.state.clickedCard);
+        if(this.state.clickedCard.length > 0){
+            // return if clicking on same card multiple times
+            if(this.state.clickedCard[1] === index){
+                console.log("-1-", "multiple clicks");
+                return;
+            }
+            // check for match
+            if(this.state.clickedCard[0] === cardImage){
+                console.log("-2-", "match");
+                this.setState({
+                    status: "match found"
+                });
+            }else{
+                this.setState({
+                    clickedCard: []
+                });                
+            }
+        }
+        //
+        else{
+            this.setState({
+                clickedCard: [cardImage, index]
+            });
+            return;
+        }
     }
 
     render(){
@@ -67,12 +97,12 @@ class Game extends React.Component{
             <div className="game">
                 <div className="game-board">
                     <Board
-                        onClick={() => this.handleClick()}
                         images={this.state.cardImages}
+                        onClick={(cardImage, index) => this.handleClick(cardImage, index)}
                     />
                 </div>
                 <div className="game-info">
-                    <div>status</div>
+                <div>status: {this.state.status}</div>
                 </div>
             </div>
         );
